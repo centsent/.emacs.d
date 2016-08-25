27afcf3bd2b1b713628c0031ad77cb534db86f5d
centsent <ivan.hui.g+d@gmail.com>
2016-08-26 00:12:23 +0800

    feat: enable git config

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,36 +1,40 @@
+;;; init-git --- Git Configurations
+;;; Commentary:
+
+;;; Code:
+
 ;; TODO: link commits from vc-log to magit-show-commit
 ;; TODO: smerge-mode
 (require-package 'git-blame)
 (require-package 'gitignore-mode)
 (require-package 'gitconfig-mode)
 (require-package 'git-messenger) ;; Though see also vc-annotate's "n" & "p" bindings
-(maybe-require-package 'git-timemachine)
+(require-package 'git-timemachine)
 
 
-(when (maybe-require-package 'magit)
+(when (require-package 'magit)
   (setq-default
-   magit-process-popup-time 10
+   ;; magit-process-popup-time 10
    magit-diff-refine-hunk t
    magit-completing-read-function 'magit-ido-completing-read)
 
   ;; Hint: customize `magit-repo-dirs' so that you can use C-u M-F12 to
   ;; quickly open magit on any one of your projects.
-  (global-set-key [(meta f12)] 'magit-status)
-  (global-set-key (kbd "C-x g") 'magit-status)
-  (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup))
+  ;; (global-set-key [(meta f12)] 'magit-status)
+  (global-set-key (kbd "C-x g") 'magit-status))
 
-(after-load 'magit
-  (define-key magit-status-mode-map (kbd "C-M-<up>") 'magit-section-up)
-  (add-hook 'magit-popup-mode-hook 'sanityinc/no-trailing-whitespace))
+;; (after-load 'magit
+;;   (add-hook 'magit-popup-mode-hook 'sanityinc/no-trailing-whitespace))
 
-(require-package 'fullframe)
-(after-load 'magit
-  (fullframe magit-status magit-mode-quit-window))
+;; (require-package 'fullframe)
+;; (after-load 'magit
+;;   (fullframe magit-status magit-mode-quit-window))
 
-(when (maybe-require-package 'git-commit)
+(when (require-package 'git-commit)
   (add-hook 'git-commit-mode-hook 'goto-address-mode))
 
 
+(defvar *is-a-mac*)
 (when *is-a-mac*
   (after-load 'magit
     (add-hook 'magit-mode-hook (lambda () (local-unset-key [(meta h)])))))
@@ -52,31 +56,32 @@
 ;;       (magit-svn-mode)))
 ;;   (add-hook 'magit-status-mode-hook #'sanityinc/maybe-enable-magit-svn-mode))
 
-(after-load 'compile
-  (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
-                      '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
-    (add-to-list 'compilation-error-regexp-alist-alist defn)
-    (add-to-list 'compilation-error-regexp-alist (car defn))))
+;; (after-load 'compile
+;;   (dolist (defn (list '(git-svn-updated "^\t[A-Z]\t\\(.*\\)$" 1 nil nil 0 1)
+;;                       '(git-svn-needs-update "^\\(.*\\): needs update$" 1 nil nil 2 1)))
+;;     (add-to-list 'compilation-error-regexp-alist-alist defn)
+;;     (add-to-list 'compilation-error-regexp-alist (car defn))))
 
-(defvar git-svn--available-commands nil "Cached list of git svn subcommands")
-(defun git-svn--available-commands ()
-  (or git-svn--available-commands
-      (setq git-svn--available-commands
-            (sanityinc/string-all-matches
-             "^  \\([a-z\\-]+\\) +"
-             (shell-command-to-string "git svn help") 1))))
+;; (defvar git-svn--available-commands nil "Cached list of git svn subcommands")
+;; (defun git-svn--available-commands ()
+;;   (or git-svn--available-commands
+;;       (setq git-svn--available-commands
+;;             (sanityinc/string-all-matches
+;;              "^  \\([a-z\\-]+\\) +"
+;;              (shell-command-to-string "git svn help") 1))))
 
-(defun git-svn (dir command)
-  "Run a git svn subcommand in DIR."
-  (interactive (list (read-directory-name "Directory: ")
-                     (completing-read "git-svn command: " (git-svn--available-commands) nil t nil nil (git-svn--available-commands))))
-  (let* ((default-directory (vc-git-root dir))
-         (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
-    (compile (concat "git svn " command))))
+;; (defun git-svn (dir command)
+;;   "Run a git svn subcommand in DIR."
+;;   (interactive (list (read-directory-name "Directory: ")
+;;                      (completing-read "git-svn command: " (git-svn--available-commands) nil t nil nil (git-svn--available-commands))))
+;;   (let* ((default-directory (vc-git-root dir))
+;;          (compilation-buffer-name-function (lambda (major-mode-name) "*git-svn*")))
+;;     (compile (concat "git svn " command))))
 
-
-(require-package 'git-messenger)
-(global-set-key (kbd "C-x v p") #'git-messenger:popup-message)
+;; 
+;; (require-package 'git-messenger)
+;; (global-set-key (kbd "C-x v p") #'git-messenger:popup-message)
 
 
 (provide 'init-git)
+;;; init-git.el ends here
--- lisp/init-github.el
+++ lisp/init-github.el
@@ -1,13 +1,17 @@
+;;; init-github --- Github Configurations
+;;; Commentary:
+
+;;; Code:
+
 (require 'init-git)
 
 (maybe-require-package 'yagist)
 (require-package 'bug-reference-github)
 (add-hook 'prog-mode-hook 'bug-reference-prog-mode)
 
-(maybe-require-package 'github-clone)
-(maybe-require-package 'github-issues)
-(maybe-require-package 'magit-gh-pulls)
-
-
+(require-package 'github-clone)
+(require-package 'github-issues)
+(require-package 'magit-gh-pulls)
 
 (provide 'init-github)
+;;; init-github.el ends here
