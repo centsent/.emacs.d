7cd515c89df9b56705dd614a63a4d96c983498b8
centsent <ivan.hui.g+d@gmail.com>
2018-06-26 16:29:22 +0800

    feat: use prettier for formatting

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -26,13 +26,9 @@
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (setup-tide-mode))))
 
 ;; enable typescript-tslint checker
-(flycheck-add-mode 'typescript-tslint 'web-mode)
+ (flycheck-add-mode 'typescript-tslint 'web-mode)
 
 (provide 'init-typescript)
 ;;; init-typescript.elends here
